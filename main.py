0ecd9528b7571350784676f72ab6798e781c3ee4
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2023-07-14 22:33:07 +0530

    day1

---
+++ main.py
@@ -0,0 +1,86 @@
+# User will input (2numbers).Write a program to swap the numbers
+# a = int(input('number a'))
+# b = int(input('number b'))
+# c = a
+# a=b
+# b= c
+# print("After swapping", "value of A is:", a, "\n" )
+# print("After swapping","value of B is:",b)
+
+
+
+# Write a program that will give you the sum of 3 digits
+
+# num=int(input('enter 3 digit number'))
+
+# sum = 0
+
+# for i in str(num):7894
+#     sum+=int(i)
+# print(sum)
+
+
+
+# Write a program that will reverse a four digit number.Also it checks whether the reverse is true.
+
+# num = (int(input('enter 4 digit number')))
+# rev=0
+# while num>0:
+#     rev=(rev*10)+(num%10); #adding each last digit at end
+#     num=num//10
+    
+    
+# print(rev)
+
+
+# Write a program that will tell whether the number entered by the user is odd or even.
+
+# def number (n):
+#     if n %2==0:
+#         print('even')
+#     else:
+#         print('odd')
+# number(5)
+
+
+
+# Write a program that will tell whether the given year is a leap year or not.
+
+# def year(n):
+#     if(n%100==0 or n%4==0 ):
+#         print("leap year")
+#     else:
+#         print('not leap')
+# year(300)
+
+# Write a program to find the euclidean distance between two coordinates
+
+# def coordiates(x1,y1 , x2,y2):
+#     dist = abs((((x1-x2)**2)+((y1-y2)**2)))
+#     final = dist**(1/2)
+#     print(final)
+# coordiates(1,2,2,1)
+
+
+
+# Angle between hour and minute hand
+# M=0
+# H=0
+# def time(H , M):
+    
+    
+#     M = M//5
+#     angle = abs((H-M)*30)
+#     print(angle)
+    
+# time(7,45)
+    
+    
+
+
+    
+# Overlapping rectangles
+
+
+    
+    
